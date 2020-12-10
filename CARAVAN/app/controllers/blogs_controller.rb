b8ef42810648d6f8da3ebb0490c3a52d67a63df6
syuntaochiai33@gmal.com <ec2-user@ip-172-31-21-226.ec2.internal>
2020-12-10 03:46:28 +0000

    [Start]3-4

--- CARAVAN/app/controllers/blogs_controller.rb
+++ CARAVAN/app/controllers/blogs_controller.rb
@@ -1,14 +1,17 @@
 class BlogsController < ApplicationController
-  def index
-  end
-
+  
   def show
   end
+  
+  def index
+    @blog = Blog.all
+  end
+  
 
   def new
     @blog = Blog.new
   end
-  
+
   def create
     blog = Blog.new(blog_params)
     blog.save
@@ -17,10 +20,10 @@ class BlogsController < ApplicationController
 
   def edit
   end
-  
+
   private
   def blog_params
     params.require(:blog).permit(:title, :category, :body)
   end
-  
+
 end
